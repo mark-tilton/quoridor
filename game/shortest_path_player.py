b8b6b5c130d7759dd6f519a1a60c9a61f53a6040
Mark Tilton <insomain@gmail.com>
2018-11-15 00:12:15 -0500

    Update comments for wall blocking

--- game/shortest_path_player.py
+++ game/shortest_path_player.py
@@ -14,15 +14,17 @@ class ShortestPathPlayer(Player):
     def take_action(self, opponent, board_state):
         if r.randint(0, 1) == 0 and self.wall_count > 0:
             # Block along opponent's shortest path
-            # Work from the opponents perspective
+            # Calculate board state and opponents position (from our perspective)
             opp_dist = board_state.get_distance_matrix_from_row(8)
             opp_pos = BoardState.flip_cell_position(opponent.pos)
+            # Play the game forward from the opponents perspective, just moving.
             for i in range(1):
                 opp_action = self.get_best_move(opp_pos, board_state, opp_dist)
                 opp_move = Move(board_state, opp_pos, opp_action)
                 board_state = opp_move.get_board_state()
                 last_pos = opp_pos
                 opp_pos = opp_action.new_pos
+            # Block the path from the last position to the latest position
             direction = opp_pos - last_pos
             orientation = 1 if direction[1] == 0 else 2
             points = BoardState.get_wall_points(last_pos, direction)
@@ -30,7 +32,7 @@ class ShortestPathPlayer(Player):
                 if (board_state.is_wall_index_in_bounds(point)
                     and board_state.walls[point[0], point[1]] == 0):
                     return create_block(point, orientation)
-
+            return None # Return None if both points are already filled.
         else:
             # Move along shortest path
             distance_matrix = board_state.get_distance_matrix_from_row(0)
